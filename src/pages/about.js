cd1d4d12f7f5c22c865abba05045783bcc35bad9
Therese C Kjær <69575527+ThereseCamK@users.noreply.github.com>
2024-09-18 14:38:16 +0200

    about

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,33 @@
 export default function About() {
     return (
-    <h1>About</h1>
+    <div> 
+        <h1> Hei, og velkommen! </h1>
+
+        <div className="infoContent">
+        Jeg heter Therese Camilla Nordnes, 
+        og jeg er lærer i IT-utvikling og Nøkkelkompetanse. 
+    
+      <div>
+      Med en brennende lidenskap for Frontend, og personlig utvikling. 
+        Siden jeg oppdaget interessen for å programmere i 2019
+        har jeg jobbet med å hjelpe studenter med å nå sine mål gjennom i både 
+        det å lære seg grunnleggende forståelse for programmering 
+        det å komme seg ut i arbeid.
+      </div>
+       
+        
+        <div>
+        Jeg tror på at tillit, trygghet og tilrettelegging, 
+        er gode og viktlige verdier for å få best mulig læring og utvikling. 
+        og legger stor vekt på å lytte til hver enkelt person. 
+        Når jeg ikke jobber, kan du ofte finne meg med familie 
+        og venner eller forran dataen med ett eget prosjekt.
+
+        Ta gjerne kontakt hvis du har et prosjekt du vil 
+        diskutere eller bare vil si hei!
+        </div>
+      
+        </div>
+    </div>
 )
   }
